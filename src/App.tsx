90897e8683664323666199859b5a4ce1c4b672b6
mag7hub <magadimane1@gmail.com>
2026-07-16 23:15:47 +0200

    made edit on App.tsx file, added route to registration page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./1pages/Login";
+import Register from "./1pages/Register";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Register />} />
     </Routes>
   );
 }
